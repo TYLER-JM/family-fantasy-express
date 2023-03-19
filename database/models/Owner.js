a37884b70b583b3bbd809422f6ee00c5a8de2d4c
Tyler Martin <tyler.martin@electric-playground.com>
2023-03-19 19:56:28 -0300

    adding 'upcomingGames' query to Owner model

--- database/models/Owner.js
+++ database/models/Owner.js
@@ -2,22 +2,40 @@ import prisma from '../prismaClient.js'
 
 
 const Owner = {
-  async seed(props) {
-    await prisma.owner.create({
-      data: {
-        name: props.name,
-        teams: {
-          create: props.teams
+  async upcomingGames(ownerId) {
+    //dynamically get start and ends of a date
+    let startOfDay = new Date()
+    startOfDay.setUTCHours(0,0,0,0)
+    let endOfDay = new Date(startOfDay.getTime())
+    endOfDay.setUTCHours(23,59,59,999)
+
+    let histories = await prisma.ownerTeamHistory.findMany({
+      where: {ownerId: ownerId, endDate: null},
+      include: {
+        team: {
+          include: {
+            homeGames: {
+              where: {
+                scheduledDate: {gte: startOfDay, lte: endOfDay}
+              },
+              include: {awayTeam: true, homeTeam: true}
+            },
+            awayGames: {
+              where: {
+                scheduledDate: {gte: startOfDay, lte: endOfDay}
+              },
+              include: {homeTeam: true, awayTeam: true}
+            }
+          }
         }
       }
     })
-  },
-  async findMany() {
-    return await prisma.owner.findMany({
-      include: {
-        teams: true
-      }
-    })
+
+    let allGames = []
+    histories.forEach(history => {
+      allGames = [...allGames, ...history.team.homeGames, ...history.team.awayGames]
+    });
+    return allGames
   }
 }
 
