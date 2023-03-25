2aa75a9adb65a3fef275ebb06000581507984a35
Tyler Martin <tyler.martin@electric-playground.com>
2023-03-24 23:07:39 -0300

    moves upcoming games to Predictions page

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -1,4 +1,3 @@
-import Owner from "../database/models/Owner.js"
 
 export default {
    get(req, res) {
@@ -8,15 +7,6 @@ export default {
     if (!templateVars.username) {
       res.redirect('/login')
     }
-    Owner.upcomingGames(req.session.ownerId).then(games => {
-      // histories.forEach(history => {
-      //   allGames = [...allGames, ...history.team.awayGames, ...history.team.homeGames]
-      // });
-      templateVars.games = games
-      return res.render('dashboard', templateVars)
-    }).catch(err => {
-      console.log('THERE WAS AN ERROR', err)
-      return res.render('dashboard', templateVars)
-    })
+    res.render('dashboard', templateVars)
   },
 }
--- controllers/PredictionsControllers.js
+++ controllers/PredictionsControllers.js
@@ -1,13 +1,22 @@
+import Owner from "../database/models/Owner.js"
+
 export default {
   get (req, res) {
     let templateVars = {
       date: req.params.date,
-      username: req.session.username
+      username: req.session.username,
     }
     if (!templateVars.username) {
       res.redirect('/login')
     }
-    res.render('predictions-create.pug', templateVars)
+    Owner.upcomingGames(req.session.ownerId).then(games => {
+      templateVars.games = games
+      templateVars.gameDate = new Date()
+      return res.render('predictions-create', templateVars)
+    }).catch(err => {
+      console.log('THERE WAS AN ERROR', err)
+      return res.render('dashboard', templateVars)
+    })
   },
   post() {
 
