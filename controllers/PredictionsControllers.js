2728d4cfa2a9ddb44b49e07a372542552b3eb6f6
Tyler Martin <tyler.martin@electric-playground.com>
2023-03-25 23:20:38 -0300

    updates views and predictions controller to display upcoming games and prepare to save predictions in dB

--- controllers/PredictionsControllers.js
+++ controllers/PredictionsControllers.js
@@ -1,4 +1,5 @@
 import Owner from "../database/models/Owner.js"
+import prisma from "../database/prismaClient.js"
 
 export default {
   get (req, res) {
@@ -10,15 +11,33 @@ export default {
       res.redirect('/login')
     }
     Owner.upcomingGames(req.session.ownerId).then(games => {
+      console.log('games: ', games)
       templateVars.games = games
-      templateVars.gameDate = new Date()
       return res.render('predictions-create', templateVars)
     }).catch(err => {
       console.log('THERE WAS AN ERROR', err)
       return res.render('dashboard', templateVars)
     })
   },
-  post() {
-
+  post(req, res) {
+    console.log('BODY', req.body)
+    for (const gameId in req.body) {
+      let [teamId, outcome] = req.body[gameId].split('-')
+      // I think I have to CreateMany
+      prisma.prediction.create({
+        data: {
+          ownerId: req.session.ownerId,
+          eventId,
+          teamId,
+          winPrediction: outcome === 'win'
+        }
+      }).then(result => {
+        console.log('created: ', result.id)
+        res.redirect('/')
+      }).catch(error => {
+        console.log("ERROR making prediction: ", error)
+        res.redirect('/')
+      })
+    }
   }
 }
--- database/models/Owner.js
+++ database/models/Owner.js
@@ -4,11 +4,12 @@ import prisma from '../prismaClient.js'
 const Owner = {
   async upcomingGames(ownerId) {
     //dynamically get start and ends of a date
+    // I just accept a date as second parameter so dates further in the future can be got
     let startOfDay = new Date()
     startOfDay.setUTCHours(0,0,0,0)
     let endOfDay = new Date(startOfDay.getTime())
     endOfDay.setUTCHours(23,59,59,999)
-
+    console.log("DAY start/end: ", startOfDay, endOfDay)
     let histories = await prisma.ownerTeamHistory.findMany({
       where: {ownerId: ownerId, endDate: null},
       include: {
@@ -18,12 +19,14 @@ const Owner = {
               where: {
                 scheduledDate: {gte: startOfDay, lte: endOfDay}
               },
+              orderBy: {scheduledDate: 'asc'},
               include: {awayTeam: true, homeTeam: true}
             },
             awayGames: {
               where: {
                 scheduledDate: {gte: startOfDay, lte: endOfDay}
               },
+              orderBy: {scheduledDate: 'asc'},
               include: {homeTeam: true, awayTeam: true}
             }
           }
@@ -31,12 +34,30 @@ const Owner = {
       }
     })
 
-    let allGames = []
-    histories.forEach(history => {
-      allGames = [...allGames, ...history.team.homeGames, ...history.team.awayGames]
-    });
+    let allGames = histories.map(history => {
+      if (history.team.homeGames.length) {
+        return {
+          homeTeam: history.team.homeGames[0].homeTeam,
+          ownedTeam: history.team.homeGames[0].homeTeam,
+          awayTeam: history.team.homeGames[0].awayTeam,
+          id: history.team.homeGames[0].id,
+          date: history.team.homeGames[0].scheduledDate
+        }
+      } else if (history.team.awayGames.length) {
+        return {
+          homeTeam: history.team.awayGames[0].homeTeam,
+          ownedTeam: history.team.awayGames[0].awayTeam,
+          awayTeam: history.team.awayGames[0].awayTeam,
+          id: history.team.awayGames[0].id,
+          date: history.team.awayGames[0].scheduledDate
+        }
+      } else {
+        return false
+      }
+    }).filter(game => game)
     return allGames
   }
 }
 
+// Owner.upcomingGames(6)
 export default Owner
